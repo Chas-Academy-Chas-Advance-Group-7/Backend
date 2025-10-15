723824c97567b8f58474bb95b47556bf41ae6e47
Sami A <alhalabisami@gmail.com>
2025-10-15 19:00:00 +0200

    changed some port logic in server.ts to get it to work on azure

--- api/src/server.ts
+++ api/src/server.ts
@@ -14,36 +14,36 @@ import packagePortal from "./routes/packageRoutes/packagePortal.js";
 console.log("Server file loading...");
 dotenv.config();
 console.log("Environment loaded:", {
-	PORT: process.env.PORT,
-	SERVER_PORT: process.env.SERVER_PORT,
-	NODE_ENV: process.env.NODE_ENV,
-	JWT_SECRET: process.env.JWT_SECRET ? "Exists" : "Missing",
+  PORT: process.env.PORT,
+  SERVER_PORT: process.env.SERVER_PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET ? "Exists" : "Missing",
 });
 
 testConnection();
 
 const app = express();
 
-const port = parseInt(process.env.PORT || "3000", 10);
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
 const swaggerOptions = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Chas-advance",
-			description: "this is the api developed during our chas-advance project",
-			version: "0.1.0",
-		},
-		servers: [
-			{
-				url: `http://localhost:${port}`,
-				description: "This is a local dev server for documentation",
-			},
-		],
-	},
-	apis: ["./routes/*.ts"],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Chas-advance",
+      description: "this is the api developed during our chas-advance project",
+      version: "0.1.0",
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+        description: "This is a local dev server for documentation",
+      },
+    ],
+  },
+  apis: ["./routes/*.ts"],
 };
 const swaggerDocs = YAML.load(path.resolve("./src/swagger/swagger.yaml"));
 
@@ -60,16 +60,16 @@ app.use("/user_portal", userPortal);
 app.use("/package_portal", packagePortal);
 
 app.get("/", (_req, res) => {
-	res
-		.status(200)
-		.json({ message: "Welcome to the internet, have a look around" });
+  res
+    .status(200)
+    .json({ message: "Welcome to the internet, have a look around" });
 });
 
-app.listen(port, "0.0.0.0", () => {
-	console.log(`Server is listening on port ${port}`);
-	console.log("ENV vars:", {
-		SERVER_PORT: process.env.SERVER_PORT,
-		PORT: process.env.PORT,
-		NODE_ENV: process.env.NODE_ENV,
-	});
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+  console.log("ENV vars:", {
+    SERVER_PORT: process.env.SERVER_PORT,
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+  });
 });
