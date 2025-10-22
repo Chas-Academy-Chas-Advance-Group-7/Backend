cae023e5860aed7dcb631b2b3a4978f748597939
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-10-22 15:00:24 +0200

    Halfway through the sensor reading post operation. Need to pause that to work with packages that come from outside the projektets omfång

--- api/src/routes/packageRoutes/sensor/sensorRoutes.ts
+++ api/src/routes/packageRoutes/sensor/sensorRoutes.ts
@@ -10,63 +10,37 @@ sensorRoute.get("/", (_req, res) => {
 });
 
 sensorRoute.post("/in_transit", async (req, res) => {
-  const { accesKey, truck_id, sensors  } = req.body
-	
-	if(!accesKey || !truck_id || sensors.isArray()){
+	const { accesKey, truck_id, sensors } = req.body;
+
+	if (!accesKey || !truck_id || sensors.isArray()) {
 		res.status(400).json({
-			"message" : "Acces key or truck_id is missing "
-		})
+			message: "Acces key or truck_id is missing ",
+		});
 		return;
 	}
 
-	if(accesKey !== process.env.SENSOR_ACCES_KEY) {
+	if (accesKey !== process.env.SENSOR_ACCES_KEY) {
 		res.status(401).json({
-			"message": "The acces key that was submitted is false, no allowance admitted"
-		})
+			message:
+				"The acces key that was submitted is false, no allowance admitted",
+		});
 		return;
 	}
 
-try {
-	const query = `INSERT INTO sensor_reading (sensor_id, timestamp, temperature, humidity) VALUES ($1, $2, $3, $4)`
-	sensors.forEach((sensor:any) => {
-		const values = [
-			sensor.sender_id,
-			sensor.data.temperature,
-			sensor.data.humidity,
-			sensor.data.timestamp
-		]
-		const insertReading = await db.pool.query(query, values)
-	});
-  
-  const 
+	try {
+		const query = `INSERT INTO sensor_reading (sensor_id, timestamp, temperature, humidity) VALUES ($1, $2, $3, $4)`;
+		sensors.forEach((sensor: any) => {
+			const values = [
+				sensor.sensor_id,
+				sensor.data.temperature,
+				sensor.data.humidity,
+				sensor.data.timestamp,
+			];
+			const insertReading = await db.pool.query(query, values);
+		});
 
-} catch (error) {
-	
-}
-
-	
-})
+		const;
+	} catch (error) {}
+});
 
 export default sensorRoute;
-{
-  "access_key": "JagHarEttSensorPaket",
-  "truck_id": "truck-001",
-  "sensors": [
-    {
-      "sensor_id": 12,
-      "data": {
-        "timestamp": 1695654381,
-        "temperature": 4.6,
-        "humidity": 77.9
-      }
-    },
-    {
-      "sensor_id": 13,
-      "data": {
-        "timestamp": 1695654321,
-        "temperature": -18.1,
-        "humidity": 75.3
-      }
-    }
-  ]
-}
