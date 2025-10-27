fb3cffb9767bab2209d0d13ad8466429dccdb64e
Sami A <alhalabisami@gmail.com>
2025-10-27 15:33:34 +0100

    the query in singel_packege route had som syntax errors, repaced some of the . to _

--- api/dist/src/routes/userRoutes/recieverRoutes/recieverRoutes.js
+++ api/dist/src/routes/userRoutes/recieverRoutes/recieverRoutes.js
@@ -33,27 +33,28 @@ recieverRoute.get("/single_package/:user_id/:package_id", authenticateJWT, async
             .json({ message: "user id or package id could not be found" });
     }
     try {
-        const query = `SELECT 
-    p.*, 
-    receiver.email AS receiver_email, 
-    receiver.user_name AS receiver_name, 
-    sender.email AS sender_email, 
-    sender.user_name AS sender_name, 
-    delivery_point.address AS delivery_point.address, 
-    delivery_point.postal_code AS delivery_point.postal_code, 
-    delivery_point.city AS delivery_point.city
-FROM 
-    package p
-JOIN 
-    users receiver ON p.receiver_id = receiver.id
-JOIN 
-    users sender ON p.sender_id = sender.id
-JOIN 
-    delivery_point ON p.delivery_point_id = delivery_point.id
-WHERE 
-    p.receiver_id = $1 
-    AND p.package_id = $2;
- `;
+        const query = `
+                SELECT 
+                    p.*, 
+                    receiver.email AS receiver_email, 
+                    receiver.user_name AS receiver_name, 
+                    sender.email AS sender_email, 
+                    sender.user_name AS sender_name, 
+                    delivery_point.address AS delivery_address, 
+                    delivery_point.postal_code AS delivery_postal_code, 
+                    delivery_point.city AS delivery_city
+                FROM 
+                    package p
+                JOIN 
+                    users receiver ON p.receiver_id = receiver.id
+                JOIN 
+                    users sender ON p.sender_id = sender.id
+                JOIN 
+                    delivery_point ON p.delivery_point_id = delivery_point.id
+                WHERE 
+                    p.receiver_id = $1 
+                    AND p.id = $2;
+            `;
         const values = [user_id, package_id];
         const jsonData = await db.pool.query(query, values);
         res.status(200).json(jsonData.rows);
--- api/src/routes/userRoutes/recieverRoutes/recieverRoutes.ts
+++ api/src/routes/userRoutes/recieverRoutes/recieverRoutes.ts
@@ -35,6 +35,7 @@ recieverRoute.get(
   async (req, res) => {
     const user_id = req.params.user_id;
     const package_id = req.params.package_id;
+
     if (!user_id || !package_id) {
       return res
         .status(401)
@@ -42,27 +43,28 @@ recieverRoute.get(
     }
 
     try {
-      const query = `SELECT 
-    p.*, 
-    receiver.email AS receiver_email, 
-    receiver.user_name AS receiver_name, 
-    sender.email AS sender_email, 
-    sender.user_name AS sender_name, 
-    delivery_point.address AS delivery_point.address, 
-    delivery_point.postal_code AS delivery_point.postal_code, 
-    delivery_point.city AS delivery_point.city
-FROM 
-    package p
-JOIN 
-    users receiver ON p.receiver_id = receiver.id
-JOIN 
-    users sender ON p.sender_id = sender.id
-JOIN 
-    delivery_point ON p.delivery_point_id = delivery_point.id
-WHERE 
-    p.receiver_id = $1 
-    AND p.package_id = $2;
- `;
+      const query = `
+                SELECT 
+                    p.*, 
+                    receiver.email AS receiver_email, 
+                    receiver.user_name AS receiver_name, 
+                    sender.email AS sender_email, 
+                    sender.user_name AS sender_name, 
+                    delivery_point.address AS delivery_address, 
+                    delivery_point.postal_code AS delivery_postal_code, 
+                    delivery_point.city AS delivery_city
+                FROM 
+                    package p
+                JOIN 
+                    users receiver ON p.receiver_id = receiver.id
+                JOIN 
+                    users sender ON p.sender_id = sender.id
+                JOIN 
+                    delivery_point ON p.delivery_point_id = delivery_point.id
+                WHERE 
+                    p.receiver_id = $1 
+                    AND p.id = $2;
+            `;
       const values = [user_id, package_id];
       const jsonData = await db.pool.query(query, values);
 
