fd700ea995195d02e623c21efe719b42b5c7a0fd
Sami A <alhalabisami@gmail.com>
2025-10-28 16:50:21 +0100

    forgot to run the build

--- api/dist/swaggerConfig.js
+++ api/dist/swaggerConfig.js
@@ -40,7 +40,7 @@ const options = {
             },
         ],
     },
-    apis: [path.resolve(__dirname, "./routes/*.ts")],
+    apis: [path.resolve(__dirname, "../src/routes/*.ts")],
 };
 const specs = swaggerJsdoc(options);
 export default specs;
